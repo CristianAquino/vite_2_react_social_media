ed04ba4ad6e87dd1b70224a99d943b7b4be7e8b6
cristianaquino <ycristian10@gmail.com>
2022-12-12 17:08:29 -0500

    posts

--- social_media/src/components/Posts/Posts.jsx
+++ social_media/src/components/Posts/Posts.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "../../https/postsRequest";
+import { postInitial } from "../../redux/slice/postsSlice";
 import { PostsData } from "../../utils/postsData";
 import Post from "../Post/Post";
 
 const Posts = () => {
+  const { posts } = useSelector((state) => state.postsSlice);
+  const { token } = useSelector((state) => state.authSlice);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getPosts(token).then((res) => dispatch(postInitial(res.data)));
+  }, []);
+
   return (
     // posts
     <div className="flex flex-col gap-[1rem]">
-      {PostsData.map((post) => (
+      {posts.map((post) => (
         <Post key={post.id} {...post} />
       ))}
     </div>

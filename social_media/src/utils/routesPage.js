ee9a53db57c7372ea8cdb3374df9194562cc83d4
cristianaquino <ycristian10@gmail.com>
2022-12-12 01:42:05 -0500

    routes

---
+++ social_media/src/utils/routesPage.js
@@ -0,0 +1,9 @@
+const routes = {
+  home: "/",
+  landingPageMusic: "/landingPageMusic",
+  landingPageFitnes: "/landingPageFitnes",
+  socialMedia: "/socialMedia",
+  error: "*",
+};
+
+export default routes;

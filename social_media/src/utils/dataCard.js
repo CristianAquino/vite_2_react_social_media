ddcf4226960375b32aad6018edcd8641c1c43e76
cristianaquino <ycristian10@gmail.com>
2022-12-15 15:48:54 -0500

    utils

---
+++ social_media/src/utils/dataCard.js
@@ -0,0 +1,22 @@
+import routes from "../utils/routesPage";
+
+const dataCard = [
+  {
+    route: routes.landingPageMusic,
+    image: "https://i.postimg.cc/1XFb4Brx/landing-Page-Music.jpg",
+    name: "landingpagemusic",
+  },
+  {
+    route: routes.landingPageFitnes,
+    image: "https://i.postimg.cc/FKtCrhkj/landing-Page-Fitnes.jpg",
+    name: "landingpagefitnes",
+  },
+  ,
+  {
+    route: routes.socialMedia,
+    image: "https://i.postimg.cc/FKtCrhkj/landing-Page-Fitnes.jpg",
+    name: "socialMedia",
+  },
+];
+
+export default dataCard;

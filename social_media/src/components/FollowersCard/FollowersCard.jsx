b1009440a4752cb9fb42ebc3880f5edcbbd58ebf
cristianaquino <ycristian10@gmail.com>
2022-12-15 14:44:41 -0500

    followers

--- social_media/src/components/FollowersCard/FollowersCard.jsx
+++ social_media/src/components/FollowersCard/FollowersCard.jsx
@@ -1,46 +1,27 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getAllDataUser } from "../../https/userRequest";
 import randomUser from "../../utils/fetch";
+import User from "../User/User";
 
 const FollowersCard = () => {
-  const [followers, setFollowers] = useState([]);
+  const { token, user } = useSelector((state) => state.authSlice);
+  const [persons, setPersons] = useState([]);
 
   useEffect(() => {
-    randomUser().then((datos) => setFollowers(datos.results));
+    // randomUser().then((datos) => setFollowers(datos.results));
+    getAllDataUser(token).then((datos) => setPersons(datos.data));
   }, []);
 
   return (
     // followersCard
     <div className="w-[100%] rounded-[0.7rem] gap-[1rem] flex flex-col text-[14px]">
-      <h3>Who is followings you</h3>
-      {followers != null &&
-        followers.map((data) => (
-          // follower
-          <div
-            key={data.name.first}
-            className="flex justify-between items-center"
-          >
-            <div className="flex gap-[10px]">
-              {/* followImg */}
-              <img
-                src={data.picture.large}
-                alt={data.name.first}
-                className="w-[3.2rem] h-[3.2rem] rounded-[50%]"
-              />
-              {/* name */}
-              <div className="flex flex-col items-start justify-center">
-                <span className="font-bold">
-                  {data.name.first + " " + data.name.last}
-                </span>
-                <span>@{data.login.username}</span>
-              </div>
-            </div>
-            {/* button */}
-            <button className="flex items-center justify-center text-white border-none rounded-[0.5rem] bg-buttonBg h-[2rem] px-[20px] hover:cursor-pointer ">
-              Follow
-            </button>
-          </div>
-        ))}
+      <h3>People you may know</h3>
+      {persons != null &&
+        persons.map((data) => {
+          if (data.id !== user.id) return <User key={data.id} data={data} />;
+        })}
     </div>
   );
 };
